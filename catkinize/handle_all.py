f6e8c4fac7d517e7d4c7cfaedd6de7541a22cdf5
Stefan Otte <stefan.otte@gmail.com>
2013-08-06 12:17:27 +0200

    Almost functional handle_all.py
    Still, this should be renamed and refactored later. But now I can use it to
    catkinize manifest files.

--- catkinize/handle_all.py
+++ catkinize/handle_all.py
@@ -1,25 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 from __future__ import print_function
 
 import sys
+
 from handle_manifest import handle_manifest
+from utils import is_valid_version
 
 
 if __name__ == "__main__":
     # TODO user arsparser
-    version = sys.args[1]
+    package_path = sys.argv[1]
+    package_name = sys.argv[2]
+    version = sys.argv[3]
+
     if not is_valid_version(version):
-        print("not a valid version")
+        print("%s is not a valid version" % version)
         sys.exit(-1)
 
-    package_path = sys.args[2]
+    possible = (
+        handle_manifest(package_path, version, dryrun=True) and
+        # handle_cmake(package_path, version, dryrun=True) and
+        # handle_make(package_path, version, dryrun=True)
+        True
+    )
 
-    # possible = handle_manifest(dryrun=True) and handle_cmake(dryrun=True) and handle_make(dryrun=True)
-
-    possible = (handle_manifest(package_path, version, dryrun=True) and
-                # handle_cmake(package_path, version, dryrun=True) and
-                # handle_make(package_path, version, dryrun=True)
-                True
-                )
     if not possible:
         print("Errors occured, aborting.")
         sys.exit(-1)
