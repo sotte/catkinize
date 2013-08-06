e97fd081720de69298aa2cd06b004ce53679ddf4
Stefan Otte <stefan.otte@gmail.com>
2013-08-06 14:34:50 +0200

    Add make_export_section()

--- catkinize/handle_manifest.py
+++ catkinize/handle_manifest.py
@@ -217,6 +217,17 @@ def make_section(tag_name, rows):
     return '\n'.join(indent(make_tag_from_row(tag_name, row)) for row in rows)
 
 
+def make_exports_section(exports, architecture_independent, metapackage):
+    parts = [make_empty_tag(name, attrs_dict)
+             for name, attrs_dict in exports]
+    if architecture_independent:
+        parts.append('<architecture_independent/>')
+    if metapackage:
+        parts.append('<metapackage/>')
+    parts = [indent(p, 2) for p in parts]
+    return '\n'.join(parts)
+
+
 def make_tag_from_row(name, row):
     """Make an XML tag from a row.
 
